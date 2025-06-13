24b958a765a6f5c624fd91b988d88d3becb2c1e1
cyanorussel <anmolrajverma@gmail.com>
2025-06-13 12:03:10 +0530

    login form

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -75,57 +75,64 @@ export default function LoginCard() {
 						sm: "400px",
 					}}
 				>
-					<Stack spacing={4}>
-						<FormControl isRequired>
-							<FormLabel>Username</FormLabel>
-							<Input
-								type='text'
-								value={inputs.username}
-								onChange={(e) => setInputs((inputs) => ({ ...inputs, username: e.target.value }))}
-							/>
-						</FormControl>
-						<FormControl isRequired>
-							<FormLabel>Password</FormLabel>
-							<InputGroup>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							handleLogin();
+						}}
+					>
+						<Stack spacing={4}>
+							<FormControl isRequired>
+								<FormLabel>Username</FormLabel>
 								<Input
-									type={showPassword ? "text" : "password"}
-									value={inputs.password}
-									onChange={(e) => setInputs((inputs) => ({ ...inputs, password: e.target.value }))}
+									type='text'
+									value={inputs.username}
+									onChange={(e) => setInputs((inputs) => ({ ...inputs, username: e.target.value }))}
 								/>
-								<InputRightElement h={"full"}>
-									<Button
-										variant={"ghost"}
-										onClick={() => setShowPassword((showPassword) => !showPassword)}
-									>
-										{showPassword ? <ViewIcon /> : <ViewOffIcon />}
-									</Button>
-								</InputRightElement>
-							</InputGroup>
-						</FormControl>
-						<Stack spacing={10} pt={2}>
-							<Button
-								loadingText='Logging in'
-								size='lg'
-								bg={useColorModeValue("gray.600", "gray.700")}
-								color={"white"}
-								_hover={{
-									bg: useColorModeValue("gray.700", "gray.800"),
-								}}
-								onClick={handleLogin}
-								isLoading={loading}
-							>
-								Login
-							</Button>
+							</FormControl>
+							<FormControl isRequired>
+								<FormLabel>Password</FormLabel>
+								<InputGroup>
+									<Input
+										type={showPassword ? "text" : "password"}
+										value={inputs.password}
+										onChange={(e) => setInputs((inputs) => ({ ...inputs, password: e.target.value }))}
+									/>
+									<InputRightElement h={"full"}>
+										<Button
+											variant={"ghost"}
+											onClick={() => setShowPassword((showPassword) => !showPassword)}
+										>
+											{showPassword ? <ViewIcon /> : <ViewOffIcon />}
+										</Button>
+									</InputRightElement>
+								</InputGroup>
+							</FormControl>
+							<Stack spacing={10} pt={2}>
+								<Button
+									loadingText='Logging in'
+									size='lg'
+									bg={useColorModeValue("gray.600", "gray.700")}
+									color={"white"}
+									_hover={{
+										bg: useColorModeValue("gray.700", "gray.800"),
+									}}
+									type="submit" // <-- Make this a submit button
+									isLoading={loading}
+								>
+									Login
+								</Button>
+							</Stack>
+							<Stack pt={6}>
+								<Text align={"center"}>
+									Don&apos;t have an account?{" "}
+									<Link color={"blue.400"} onClick={() => setAuthScreen("signup")}>
+										Sign up
+									</Link>
+								</Text>
+							</Stack>
 						</Stack>
-						<Stack pt={6}>
-							<Text align={"center"}>
-								Don&apos;t have an account?{" "}
-								<Link color={"blue.400"} onClick={() => setAuthScreen("signup")}>
-									Sign up
-								</Link>
-							</Text>
-						</Stack>
-					</Stack>
+					</form>
 				</Box>
 			</Stack>
 		</Flex>
